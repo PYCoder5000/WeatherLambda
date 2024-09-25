dcffb29a0784e3b1d92892dcbc4a31d041c02d91
PythonCoder5000 <chrisbobacat@gmail.com>
2024-09-24 19:53:52 -0700

    Undummified the code(So it actually does something)

--- index.py
+++ index.py
@@ -1,6 +1,28 @@
+import requests
+import boto3
+import time
 def handler(event, context):
-    print("Hello from Lambda!")
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda!'
-    }
+    city = "London"
+    data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=ec1e9852707e1ebb802ad2b06b2ad5dc")
+    if data.status_code == 200:
+        data = data.json()
+        weather_main = data["weather"][0]["main"]
+        temperature = data["main"]["temp"]
+        dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+        table = dynamodb.Table('WebStacklambdaStack294303EF-Weather812728CF-U3726M1L0A53')
+        table.put_item(
+            Item={
+                'location': city,
+                'timestamp': str(time.time()),
+                'weather_main': weather_main,
+                'temp': str(temperature),
+            }
+        )
+        return {
+            'statusCode': 200,
+            'body': 'YARRRRRR!'
+        }
+    else:
+        print("OH NO THE THING BLEW UP! PLEASE CONTACT ME AND I WILL RESPOUND IN...4 HUNDRED YEARS.")
+if __name__ == "__main__":
+    handler("Yup", "SAYYY WHAT")
